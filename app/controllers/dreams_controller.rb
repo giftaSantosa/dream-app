96909a146fdbb885afc33392b61612a7b259fe42
Molika <molika.haikal@gmail.com>
2026-08-18 14:01:05 +0900

    index.erb

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,16 +1,26 @@
 class DreamsController < ApplicationController
   def index
+    @dreams = Dream.all
   end
 
   def new
+    @dream = Dream.new
   end
 
   def create
+    @dream = Dream.new(dream_params)
+    if @dream.save
+      redirect_to dreams_path
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @dream = Dream.find(params[:id])
   end
 
   def update
+    params.require(:dream).permit(:title)
   end
 end

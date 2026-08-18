8d021a05dd48474cfdef1750fd5430d09dadef2e
Gifta Santosa <giftaputri.santosa@gmail.com>
2026-08-18 11:54:58 +0900

    creating both message controller actions on chat and create

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,30 @@
 class MessagesController < ApplicationController
+  before_action :set_dream
   def create
+    @message = Message.new(message_params)
+    @message.dream = @dream
+    @message.role = "user"
+    if @message.save!
+      redirect_to chat_dream_path(@dream)
+    else
+      render "chat", status: :unprocessable_entity
+    end
   end
 
+  # /dreams/:id/chat
   def chat
+    @message = Message.new
+    @messages = @dream.messages
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content)
+  end
+
+  def set_dream
+    id = params[:dream_id] || params[:id]
+    @dream = Dream.find(id)
   end
 end

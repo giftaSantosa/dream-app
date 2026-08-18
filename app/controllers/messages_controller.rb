0f8c079a9af352d95677dea87727cf340bbaa897
Gifta Santosa <giftaputri.santosa@gmail.com>
2026-08-18 12:56:25 +0900

    added llm chatbot

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,27 +1,39 @@
 class MessagesController < ApplicationController
   before_action :set_dream
+
+  PROMPT = "You are an experienced dream analyzer. You combine both mystical and science in your interpretations. You are concise.
+  I will give you an interpreted dream, and I will ask you follow up questions about it. Answer with a sentence that is max 180 characters"
+
+  INPTERPRETED_DREAM = "You are stressed, you need to take longer hours in your dream"
+
   def create
     @message = Message.new(message_params)
     @message.dream = @dream
     @message.role = "user"
 
     if @message.save!
-      # chat = RubyLLM.chat
-      # response = chat.with_instruction(Dream.system_prompt)
-      # Message.create {
-      #   content: response.content,
-      #   role: "assistant"
-      # )
+      chat = RubyLLM.chat
+      chat.with_instructions("#{PROMPT}. This is the interpreted dream #{INPTERPRETED_DREAM}")
+      response = chat.ask(@message.content)
+      ai_message = Message.new(
+        content: response.content,
+        role: "assistant",
+        dream: @dream
+      )
+      ai_message.save
       redirect_to chat_dream_path(@dream)
     else
       render "chat", status: :unprocessable_entity
     end
+
   end
 
   # /dreams/:id/chat
   def chat
     @message = Message.new
-    @messages = @dream.messages
+    @messages = @dream.messages.order(:created_at)
+    @user_messages = @dream.messages.where(role: "user")
+    @asssistant_messages = @dream.messages.where(role: "assistant")
   end
 
   private

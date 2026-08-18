8a08a5e7694cdb4f783cf31c53aaf814ad1b053c
Cassio Kiyoshi <cassiokiyoshi@icloud.com>
2026-08-18 12:15:21 +0900

    new create and show without llm

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -1,16 +1,33 @@
 class DreamsController < ApplicationController
+  # skip_before_action :authenticate_user!
   def index
   end
 
   def new
+    @dream = Dream.new
   end
 
   def create
+    @dream = Dream.new(dream_params)
+    @dream.user = User.first
+
+    if @dream.save
+      redirect_to dream_path(@dream)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def show
+    @dream = Dream.find(params[:id])
   end
 
   def update
+  end
+
+  private
+
+  def dream_params
+    params.require(:dream).permit(:input, :date, :mood)
   end
 end
